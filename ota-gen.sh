106f80446803076ab2e3e54079eb350cd3af99b0
mracar07 <32598079+mracar07@users.noreply.github.com>
2018-04-22 19:44:09 +0300

    Update ota-gen.sh

--- ota-gen.sh
+++ ota-gen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-device=$1
-cd ~/rr-o/ota/akhil
+device=z2_row
+cd ~/android/rr/ota/
 oldzip=$(grep "<Filename>RR-O" $device.xml  | sed -e 's|</\?Filename>||g')
 newzip=$2
 sed -i "s/$oldzip/$newzip/g" $device.xml
 cd -
-cd ~/rr-o/ota
+cd ~/android/rr/ota
 echo "OTA Update for RR $device $(date +%Y%m%d) Build" > /tmp/rrota
 git commit -as -F /tmp/rrota
 git pull --no-edit origin master;
